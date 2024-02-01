3e57a8c1100b4c5730acd5500593819322df2907
Eric <ericjesus403@gmail.com>
2024-02-01 11:16:27 -0300

    feat: Implementando mensagem de sucesso após criar um gênero

--- genres/views.py
+++ genres/views.py
@@ -11,6 +11,13 @@ class GenreCreateListView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializers
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(data=({'message': 'Genero criado com Sucesso'}, serializer.data), status=status.HTTP_201_CREATED, headers=headers)
+
 
 # lISTANDO OS GÊNEROS
 '''@csrf_exempt
