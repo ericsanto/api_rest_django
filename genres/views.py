fd889d2426249e93cd703a71fd9fbe7885497239
Eric <ericjesus403@gmail.com>
2024-02-01 09:40:43 -0300

    style: Melhorando o código views

--- genres/views.py
+++ genres/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+
 from .models import *
-import json
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from genres.serializers import *
 
