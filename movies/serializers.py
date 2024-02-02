491dec839358537f018c478077dbaa2b14db77e9
Eric <ericjesus403@gmail.com>
2024-02-02 10:48:53 -0300

    feat: Implementando validações no app movies no arquivo serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,7 +3,19 @@ from movies.models import Movie
 
 
 class MovieSerializers(serializers.ModelSerializer):
-  
-  class Meta:
-    model = Movie
-    fields = '__all__'
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError(
+                'Resumo excedeu o limite máximo de caractere. Limite máximo: 200 caracteres')
+        return value
+
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                'O título do filme, deve ter, pelo menos, 3 caracteres')
+        return value
