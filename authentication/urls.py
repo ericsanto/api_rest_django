cd77a4ebd0dc0aa1193ac59ff1c9ee196d318663
Eric <ericjesus403@gmail.com>
2024-02-05 10:12:41 -0300

    feat: Implementando urls para a verfificação e refresh token api

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,10 @@ from rest_framework_simplejwt.views import *
 urlpatterns = [
     path('authentication/token/', TokenObtainPairView.as_view(),
          name='authentication_obtain_token'),
+    path('authentication/token/refresh/', TokenRefreshView.as_view(),
+         name='authentication_token_refresh'),
+    path('authentication/token/verify/', TokenVerifyView.as_view(),
+         name='authentication_verify_view'),
 
 
 
